b1d156b775b8bb76b78c89de0405d6b29acaa4f6
iceout <ice.404.out@gmail.com>
2024-05-13 14:38:57 +0800

    Fix version of hop

--- dot_config/nvim/lua/plugins/hop.lua
+++ dot_config/nvim/lua/plugins/hop.lua
@@ -1,7 +1,7 @@
 return {
     {
         'smoka7/hop.nvim',
-        branch = 'v2',
+        tag = '^2',
         keys = {
             {'<leader>h', '<Cmd>HopWord<CR>', mode = 'n', silent = true},
             {'<leader>H', '<Cmd>HopLine<CR>', mode = 'n', silent = true},
