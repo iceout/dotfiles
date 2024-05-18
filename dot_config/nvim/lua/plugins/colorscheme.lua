cb61c77076c222a7bbd820625b360ed4a2583c0e
iceout <ice.404.out@gmail.com>
2024-05-18 21:56:02 +0800

    Update colorscheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -9,18 +9,18 @@ return {
       solarized.setup({
         transparent = true,
         palette = "selenized", -- or solarized
-        colors = {
-          base0 = "#b9b9b9",
-          base02 = "#3b3b3b",
-          base03 = "#181818",
-          base04 = "#252525",
-          magenta = "#eb6eb7",
-          red = "#ed4a46",
-          yellow = "#dbb32d",
-          green = "#70b433",
-          blue = "#368aeb",
-          cyan = "#3fc5b7",
-        },
+        --  colors = {
+        --    base0 = "#b9b9b9",
+        --    base02 = "#3b3b3b",
+        --    base03 = "#181818",
+        --    base04 = "#252525",
+        --    magenta = "#eb6eb7",
+        --    red = "#ed4a46",
+        --    yellow = "#dbb32d",
+        --    green = "#70b433",
+        --    blue = "#368aeb",
+        --    cyan = "#3fc5b7",
+        --  },
       })
 
       vim.cmd.colorscheme("solarized")
