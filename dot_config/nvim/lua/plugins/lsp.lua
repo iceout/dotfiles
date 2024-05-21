aaf4abd0fe1757af50786a2578ae271a99e8cf40
iceout <ice.404.out@gmail.com>
2024-05-21 17:35:15 +0800

    UP

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,70 +1,72 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-      }
-    }
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "pyright",
-        "lua_ls",
-        "gopls",
-      }
-    }
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"pyright",
+				"lua_ls",
+				"gopls",
+			},
+		},
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require('lspconfig')
-      lspconfig.pyright.setup {
-        settings = {
-          python = {
-            analysis = {
-              autoSearchPaths = false,
-            },
-          },
-        },
-      }
-      lspconfig.lua_ls.setup({})
-      lspconfig.gopls.setup({})
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.pyright.setup({
+				settings = {
+					python = {
+						analysis = {
+							autoSearchPaths = false,
+						},
+					},
+				},
+			})
+			lspconfig.lua_ls.setup({})
+			lspconfig.gopls.setup({})
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = {
-              "c",
-              "bash",
-              "html",
-              "javascript",
-              "lua",
-              "json",
-              "markdown",
-              "markdown_inline",
-              "python",
-              "tsx",
-              "typescript",
-              "yaml",
-            },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"bash",
+					"go",
+					"gotml",
+					"html",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"tsx",
+					"typescript",
+					"yaml",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
