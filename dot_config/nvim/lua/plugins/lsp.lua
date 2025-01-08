529856a5fa979a651b5687fbd6109a756f6be539
iceout <ice.404.out@gmail.com>
2025-01-08 15:14:53 +0800

    Add ruff in lspconfig

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,138 +1,150 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
-                },
-            },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
         },
+      },
     },
+  },
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "pyright",
-                "lua_ls",
-                "gopls",
-                "bashls",
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+        "ruff",
+        "lua_ls",
+        "gopls",
+        "bashls",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({
+        settings = {
+          -- Using Ruff's import organizer
+          disableOrganizeImports = true,
+          python = {
+            analysis = {
+              autoSearchPaths = true,
             },
+          },
         },
-    },
+      })
+      lspconfig.ruff.setup {
+        trace = 'messages',
+        init_options = {
+          settings = {
+            logLevel = 'debug',
+          }
+        }
+      }
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.pyright.setup({
-                settings = {
-                    python = {
-                        analysis = {
-                            autoSearchPaths = false,
-                        },
-                    },
-                },
-            })
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        runtime = {
-                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim).
-                            version = "LuaJIT",
-                        },
-                        diagnostics = {
-                            -- Get the language server to recognize the `vim` global
-                            globals = { "vim" },
-                        },
-                        workspace = {
-                            -- Make the server aware of Neovim runtime files.
-                            library = vim.api.nvim_get_runtime_file("", true),
-                        },
-                        telemetry = {
-                            enable = false, -- Disable telemetry
-                        },
-                    },
-                },
-            })
-            lspconfig.gopls.setup({})
-            lspconfig.bashls.setup({})
-        end,
-    },
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            configs.setup({
-                ensure_installed = {
-                    "c",
-                    "bash",
-                    "go",
-                    "gotmpl",
-                    "html",
-                    "javascript",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "python",
-                    "tsx",
-                    "typescript",
-                    "yaml",
-                },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
-    },
-
-    {
-        "folke/trouble.nvim",
-        tag = "v3.6.0",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        cmd = "Trouble",
-        keys = {
-            {
-                "<leader>xx",
-                "<cmd>Trouble diagnostics toggle<cr>",
-                desc = "Diagnostics (Trouble)",
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim).
+              version = "LuaJIT",
             },
-            {
-                "<leader>xX",
-                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                desc = "Buffer Diagnostics (Trouble)",
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
             },
-            {
-                "<leader>cs",
-                "<cmd>Trouble symbols toggle focus=false<cr>",
-                desc = "Symbols (Trouble)",
+            workspace = {
+              -- Make the server aware of Neovim runtime files.
+              library = vim.api.nvim_get_runtime_file("", true),
             },
-            {
-                "<leader>cl",
-                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                desc = "LSP Definitions / references / ... (Trouble)",
+            telemetry = {
+              enable = false, -- Disable telemetry
             },
-            {
-                "<leader>xL",
-                "<cmd>Trouble loclist toggle<cr>",
-                desc = "Location List (Trouble)",
-            },
-            {
-                "<leader>xQ",
-                "<cmd>Trouble qflist toggle<cr>",
-                desc = "Quickfix List (Trouble)",
-            },
+          },
         },
-        config = function()
-            require("trouble").setup {
-                position = "bottom", -- 列表显示的位置：bottom, top, left, right
-            }
-        end
+      })
+      lspconfig.gopls.setup({})
+      lspconfig.bashls.setup({})
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+        ensure_installed = {
+          "c",
+          "bash",
+          "go",
+          "gotmpl",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "tsx",
+          "typescript",
+          "yaml",
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    tag = "v3.6.0",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
     },
+    config = function()
+      require("trouble").setup {
+        position = "bottom", -- 列表显示的位置：bottom, top, left, right
+      }
+    end
+  },
 }
